d64977634a6131e931132fe4ffd6f099028831ac
lolo90400 <142466617+lolo90400@users.noreply.github.com>
2024-02-01 22:36:19 -0600

    Parte dos finalizada

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 from datasets import load_dataset
 import numpy as np 
+
 dataset = load_dataset("mstz/heart_failure")
 
 data = dataset["train"]
@@ -12,3 +13,23 @@ promedio_de_edad = np.mean(edades)
 
 print(f"El promedio de la edad de los participantes es,{promedio_de_edad}")
 
+#PARTE 2 
+
+import pandas as pd 
+
+df = pd.DataFrame(data)
+df_1 = df.query("is_dead==1")
+print(f"La cantidad de pacientes muertos es: {df_1.shape[0]}")
+df_2 = df.query("is_dead==0")
+print(f"La cantidad de pacientes vivos es: {df_2.shape[0]}")
+
+promedio_df_1 = df_1["age"].mean()
+print(F"El promedio de la edad de las  personas muertas es: {round(promedio_df_1,2)}")
+promedio_df_2 = df_2["age"].mean()
+print(f" El promedio de la edad de las personas vivas es: {round(promedio_df_2,2)}")
+
+
+
+
+
+ 
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 PARTE1: Se carga el dataset, se crea una array con ' age ' y promediamos las edades con una funcion de numpy
+
+PARTE 2: Se paso de un array a un data frame de pandas, luego creamos dos columnas de las personas con vida y son vida por ultimo promediamos sus edades
