82d82022a21bab5a7ece3dd9a423075b1f996a56
lolo90400 <142466617+lolo90400@users.noreply.github.com>
2024-02-20 21:24:53 -0600

    Parte 8 done

--- graficos.py
+++ graficos.py
@@ -65,3 +65,32 @@ fig.legend(['Hombres', 'Mujeres'], loc='upper right', fontsize='small')
 
 fig.savefig('comparacion_por_genero_.png')
 plt.show()
+
+#PARTE 8 
+
+sns.set(style="whitegrid")
+fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+def plot_pie(ax, data, labels_map, title):
+    values = data.value_counts()
+    labels = [labels_map[x] for x in values.index]
+    ax.pie(values, labels=labels, autopct='%1.1f%%', startangle=90, colors=sns.color_palette('pastel'))
+    ax.set_title(title)
+
+labels_map_anemicos = {0: 'No', 1: 'Si'}
+plot_pie(axs[0, 0], df['anaemia'], labels_map_anemicos, 'Anemicos')
+
+labels_map_diabetes = {0: 'No', 1: 'Si'}
+plot_pie(axs[0, 1], df['diabetes'], labels_map_diabetes, 'Diabeticos')
+
+labels_map_fumador = {0: 'No', 1: 'Si'}
+plot_pie(axs[1, 0], df['smoking'], labels_map_fumador, 'Fumadores')
+
+labels_map_muertos = {0: 'No', 1: 'Si'}
+plot_pie(axs[1, 1], df['DEATH_EVENT'], labels_map_muertos, 'Fallecidos')
+
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
+fig.suptitle('Diagramas de Torta por Característica', fontsize=16)
+
+fig.savefig('graficos_tortas.png')
+plt.show()
